b5e8a5b79576a6ee0fa4413870e0e9283d53b00e
Dorota Szymanska <szdorota392@gmail.com>
2023-03-12 15:49:39 +0100

    first commit

--- src/main/java/com/sda/springminiproject/SpringminiprojectApplication.java
+++ src/main/java/com/sda/springminiproject/SpringminiprojectApplication.java
@@ -3,11 +3,22 @@ package com.sda.springminiproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileNotFoundException;
+
 @SpringBootApplication
 public class SpringminiprojectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringminiprojectApplication.class, args);
+	public static void main(String[] args) throws FileNotFoundException {
+
+//        Scanner odczyt = new Scanner(new File("file1.json"));
+//
+//        String text = odczyt.nextLine();
+//        System.out.println(text);
+
+		//JsonNode jsonTree = new ObjectMapper().readTree(new File("src/main/resources/orderLines.json"));
+
+
+
 	}
 
 }
